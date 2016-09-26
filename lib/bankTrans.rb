86c555afd2126657dba9b669f5a16432d6e4c30a
Jonalogy <jonalogy@gmail.com>
2016-09-26 21:30:34 +0800

    Challenge 3 completed

--- lib/bankTrans.rb
+++ lib/bankTrans.rb
@@ -12,3 +12,36 @@
       # Are you done?
       # yes
       # Thank you!
+
+@bal = 4000
+def atm
+  puts 'What would you like to do (deposit, withdraw, check_balance)'
+  option = gets.chomp
+
+  if option == 'deposit'
+    deposit(@bal)
+  elsif option == 'withdraw'
+    withdraw(@bal)
+  elsif option == 'check_balance'
+    puts "You current balance is #{@bal}"
+  else
+    puts "Error, please check you spelling"
+  end
+end
+def deposit(bal)
+  puts "Your current balance is #{bal}"
+  puts "How much would you like to deposit?"
+  amt = gets.chomp.to_f
+  puts "You entered: #{amt}"
+  ttl = amt + bal
+  print "Your current balance is #{ttl}"
+end
+def withdraw(bal)
+  puts "Your current balance is #{bal}"
+  puts "How much would you like to withdraw?"
+  amt = gets.chomp.to_f
+  puts "You entered: #{amt}"
+  ttl = bal - amt
+  puts "Your balance is #{ttl}"
+end
+atm()
